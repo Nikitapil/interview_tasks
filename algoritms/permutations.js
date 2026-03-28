602e2f82734f343c7aa4c1765ac3d79ac68274b5
Nikitapil <nikitapiliavets@gmail.com>
2026-03-28 21:32:17 +0300

    add combinations function

--- algoritms/permutations.js
+++ algoritms/permutations.js
@@ -19,3 +19,25 @@ const permutations = (arr) => {
 }
 
 console.log(permutations([1, 2, 3]));
+
+const combinations = (arr, k) => {
+  const result = [];
+
+  const backtrack = (start = 0, current = []) => {
+    if (current.length === k) {
+      result.push([...current]);
+      return;
+    }
+
+    for (let i = start; i < arr.length; i++) {
+      current.push(arr[i]);
+      backtrack(i+1, current);
+      current.pop()
+    }
+  }
+
+  backtrack()
+  return result
+}
+
+console.log(combinations([1, 2, 3], 2));
